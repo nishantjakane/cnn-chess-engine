73bea68df5cce156e8d5552c1b5d6d8e9d69e048
Nishant212121 <savitarjakane@gmail.com>
2026-06-15 20:01:18 +0530

    refac: changed the dataset being used

--- src/dataset.py
+++ src/dataset.py
@@ -2,61 +2,48 @@ import chess
 from encoder import board_to_tensor
 import torch
 from torch.utils.data import Dataset
-import pandas as pd
-
 
 
 class ChessDataset(Dataset):
-    def __init__(self,df):
+    def __init__(self, df):
         self.df = df
 
     def __len__(self):
         return len(self.df)
 
-    def __getitem__(self,idx):
+    def __getitem__(self, idx):
         row = self.df.iloc[idx]
-        fen = row["fen"]
-        cp = row["cp"]
-        mate = row["mate"]
-        
+
+        fen = row["FEN"]
+        evaluation = row["Evaluation"]
+
         board = chess.Board(fen)
-        
+
         tensor = board_to_tensor(board)
 
-        target = torch.tensor(evaluation_to_target(cp,mate),dtype=torch.float32)
+        target = torch.tensor(
+            evaluation_to_target(evaluation),
+            dtype=torch.float32
+        )
 
-        return tensor , target
-
-def evaluation_to_target(cp,mate):
-    sign =1
-    eval_cp =0
+        return tensor, target
 
 
-    if mate is not None:
-        if mate < 0 :
-            sign =-1
-        eval_cp = sign * (1000 - abs(mate))
+def evaluation_to_target(eval_str):
+    eval_str = str(eval_str)
+
+    if eval_str.startswith("#"):
+        mate_distance = int(eval_str[1:])
+
+        sign = 1
+        if mate_distance < 0:
+            sign = -1
+
+        eval_cp = sign * (1000 - abs(mate_distance))
+
     else:
-        eval_cp =cp
-    
-    eval_cp = max(-1000,min(1000,eval_cp))
-    target = eval_cp/1000.0
-    return target
+        eval_cp = int(eval_str)
 
-import pandas as pd
+    eval_cp = max(-1000, min(1000, eval_cp))
 
-df = pd.DataFrame([
-    {
-        "fen": chess.STARTING_FEN,
-        "cp": 47,
-        "mate": None
-    }
-])
-
-dataset = ChessDataset(df)
-
-tensor, target = dataset[0]
-
-print(tensor.shape)
-print(target)
-print(type(target))
+    return eval_cp / 1000.0
---
+++ src/train.py
@@ -0,0 +1,5 @@
+from model import ChessCNN
+
+model = ChessCNN()
+
+
